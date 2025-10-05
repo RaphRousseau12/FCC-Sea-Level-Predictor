1d0fdf71cef746b7b246c28f8e7a0c7b13fddc88
Raphaël Rousseau <rousseauraphael5@gmail.com>
2025-10-05 23:51:03 +0200

    Update sea_level_predictor.py

--- code/sea_level_predictor.py
+++ code/sea_level_predictor.py
@@ -35,7 +35,7 @@ def draw_plot():
     ax.set_xlabel('Year')
     ax.set_ylabel('Sea Level (inches)')
     ax.set_title('Rise in Sea Level')
-    ax.legend(['Line of Best Fit (All Data)', 'Line of Best Fit (Since 2000)', 'Observed Data'])
+    ax.legend(['Observed Data', 'Line of Best Fit (All Data)', 'Line of Best Fit (Since 2000)'])
 
     
     # Save plot and return data for testing (DO NOT MODIFY)
